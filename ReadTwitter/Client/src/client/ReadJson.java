09f914473c1ec90777cdaa115e450f394ebb6560
nourhan <nourhan_elshenawy@hotmail.com>
2016-06-03 02:09:43 +0300

    exported top 5 hashtags per users

--- ReadTwitter/Client/src/client/ReadJson.java
+++ ReadTwitter/Client/src/client/ReadJson.java
@@ -14,12 +14,19 @@ import java.util.Iterator;
 
 import java.util.Map;
 
+import java.util.Set;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class ReadJson {
+    
+    private int[] array;
+        private int[] tempMergArr;
+        private int length;
+        
     public static void main(String[] args) {
         
         HashMap<Long, TwitterUser> hm = new HashMap<Long, TwitterUser>();
@@ -46,9 +53,9 @@ JSONParser parser = new JSONParser();
             JSONArray hashtags = (JSONArray) entities.get("hashtags");
                     Iterator i = hashtags.iterator();
             ArrayList<String> tags = new ArrayList<String>();
-            if(userID == 1538983531){
-                System.out.println("ID: " +userID);
-            }
+//            if(userID == 1538983531){
+//                System.out.println("ID: " +userID);
+//            }
                     while (i.hasNext()) {
 //                System.out.println("SIZE: " + hashtags.size());
 //                for(int i=0; i<hashtags.size(); i++){
@@ -74,7 +81,7 @@ JSONParser parser = new JSONParser();
 
         }
             
-            System.out.println("HashMap:\n"+ hm.get(1538983531L).toString());
+//            System.out.println("HashMap:\n"+ hm.get(1538983531L).toString());
 		
 
 	} catch (FileNotFoundException e) {
@@ -102,31 +109,136 @@ JSONParser parser = new JSONParser();
 //            }
 //     }
         
+                        
+        
         for (Map.Entry<Long, TwitterUser> entry : hm.entrySet()) {
          Long key = entry.getKey();
          TwitterUser value = entry.getValue();
          String textName = value.getName();
          ArrayList<String> textTags = value.getHashtags();
+            
+            if(textTags.size()>5){
+         int[] sorting = new int[textTags.size()] ;
+            ArrayList<String> temp = new ArrayList<String>();
          
-         String location = "C:\\Users\\nourhan\\Desktop\\TestingTags\\" + key + "Tags.txt";         
+         for(int i=0; i<textTags.size(); i++){
+             
+         sorting[i] = tagFrequency(textTags.get(i),textTags);
+         }
+            
+         ReadJson mms = new ReadJson();
+         mms.sort(sorting);
+            
+         for(int i =0; i<5; i++){
+             for(int j=0; j<textTags.size(); j++){
+                 if(sorting[i] == tagFrequency(textTags.get(j),textTags)){
+                     temp.add((String)textTags.get(j));
+                 }
+             }
+         }
+            
+         String location2 = "C:\\Users\\nourhan\\Desktop\\TestingTopTags\\" + key + "Top5Tags.txt";         
 
             try {
-                BufferedWriter out = new BufferedWriter(new FileWriter(location));
+                BufferedWriter out2 = new BufferedWriter(new FileWriter(location2));
                 
-                for(int i=0; i<textTags.size(); i++){
-                    out.write(textTags.get(i));
-                    out.newLine();
+                
+                
+                for(int h=0; h<5; h++){
+                    String y = temp.get(h);
+                    out2.write(y + "," + sorting[h]);
+                    out2.newLine();
                 }
-                System.out.println(textTags);
-                out.close();
+                out2.close();
             } catch (IOException e) {
             }
+            
+         }
+         
+//         String location = "C:\\Users\\nourhan\\Desktop\\TestingTags\\" + key + "Tags.txt";         
+//
+//            try {
+//                BufferedWriter out = new BufferedWriter(new FileWriter(location));
+//                if(textTags.size()>5){
+//                }
+//                for(int i=0; i<textTags.size(); i++){
+//                    out.write(textTags.get(i));
+//                    out.newLine();
+//                }
+//                System.out.println(textTags);
+//                out.close();
+//            } catch (IOException e) {
+//            }
      }
      
      
      
      
      }
+    
+    public static int tagFrequency(String word, ArrayList<String> t){
+        
+        int k=0;
+        
+        for(String c: t){
+                if(c.compareToIgnoreCase(word) == 0) {
+                    k= k+1;
+                }
+        }
+        System.out.println(k + "KKKKKKKKKK");
+        return k;
+        
+        
+    }
+    
+    
+    
+    public void sort(int inputArr[]) {
+        this.array = inputArr;
+        this.length = inputArr.length;
+        this.tempMergArr = new int[length];
+        doMergeSort(0, length - 1);
+    }
+    
+    private void doMergeSort(int lowerIndex, int higherIndex) {
+         
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            // Below step sorts the left side of the array
+            doMergeSort(lowerIndex, middle);
+            // Below step sorts the right side of the array
+            doMergeSort(middle + 1, higherIndex);
+            // Now merge both sides
+            mergeParts(lowerIndex, middle, higherIndex);
+        }
+    }
+    
+    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
+    
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            tempMergArr[i] = array[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (tempMergArr[i] <= tempMergArr[j]) {
+                array[k] = tempMergArr[i];
+                i++;
+            } else {
+                array[k] = tempMergArr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= middle) {
+            array[k] = tempMergArr[i];
+            k++;
+            i++;
+        }
+    
+    }
+  
 
 }
     
